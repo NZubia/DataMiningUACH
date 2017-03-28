87b575e26e3db9b90ca7ea178a60ab38a3efbc17
Normando Ali Zubia Hernández <nzubia@blacklabs.mx>
2017-03-28 15:32:26 -0700

    Se agrego la capacidad de elegir las mejores características usando árboles de decisión

---
+++ dimensionality_reduction.py
@@ -0,0 +1,93 @@
+"""
+Author: Normando Ali Zubia Hernández
+
+This file is created to explain the use of dimensionality reduction
+with different tools in sklear library.
+
+Every function contained in this file belongs to a different tool.
+"""
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+
+
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
+
+def principal_components_analysis(n_components):
+    # import data
+    iris = datasets.load_iris()
+    X = iris.data
+    Y = iris.target
+
+    # First 10 rows
+    print('Training Data:\n\n' + str(X[:10]))
+    print('\n')
+    print('Targets:\n\n' + str(Y[:10]))
+
+    # Model declaration
+    if n_components < 1:
+        pca = PCA(n_components=n_components, svd_solver='full')
+    else:
+        pca = PCA(n_components=n_components)
+
+    # Model training
+    pca.fit(X)
+
+    # Model transformation
+    new_feature_vector = pca.transform(X)
+
+    # Model information:
+    print('\nModel information:\n')
+    print('Number of components elected: ' + str(pca.n_components))
+    print('New feature dimension: ' + str(pca.n_components_))
+    print('Variance of every feature: ' + str(pca.explained_variance_ratio_))
+
+    # First 10 rows of new feature vector
+    print('\nNew feature vector:\n')
+    print(new_feature_vector[:10])
+
+    # Print complete dictionary
+    # print(pca.__dict__)
+
+
+def attribute_subset_selection_with_trees():
+    # import data
+    iris = datasets.load_iris()
+    X = iris.data
+    Y = iris.target
+
+    # First 10 rows
+    print('Training Data:\n\n' + str(X[:10]))
+    print('\n')
+    print('Targets:\n\n' + str(Y[:10]))
+
+    # Model declaration
+    extra_tree = ExtraTreesClassifier()
+
+    # Model training
+    extra_tree.fit(X, Y)
+
+    # Model information:
+    print('\nModel information:\n')
+
+    # display the relative importance of each attribute
+    print('Importance of every feature: ' + str(extra_tree.feature_importances_))
+
+    #If model was training before prefit = True
+    model = SelectFromModel(extra_tree, prefit=True)
+
+    # Model transformation
+    new_feature_vector = model.transform(X)
+
+    # First 10 rows of new feature vector
+    print('\nNew feature vector:\n')
+    print(new_feature_vector[:10])
+
+
+if __name__ == '__main__':
+    # principal_components_analysis(2)
+    # principal_components_analysis(.93)
+
+    attribute_subset_selection_with_trees()
